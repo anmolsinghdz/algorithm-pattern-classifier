1329977d17e1316ebd32a3e530fbc4d680e71fe3
Priyanshu <pv254424@gmail.com>
2026-07-27 17:02:58 +0530

    feat: implement dynamic programming pattern detector

--- src/algorithm_pattern_classifier/detectors/__init__.py
+++ src/algorithm_pattern_classifier/detectors/__init__.py
@@ -1,6 +1,7 @@
 """Detectors sub-package."""
 
+from algorithm_pattern_classifier.detectors.dynamic_programming import DynamicProgrammingDetector
 from algorithm_pattern_classifier.detectors.sliding_window import SlidingWindowDetector
 from algorithm_pattern_classifier.detectors.two_pointer import TwoPointerDetector
 
-__all__ = ["SlidingWindowDetector", "TwoPointerDetector"]
+__all__ = ["DynamicProgrammingDetector", "SlidingWindowDetector", "TwoPointerDetector"]
